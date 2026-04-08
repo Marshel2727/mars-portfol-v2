94783caed092a2a70bd16cc6fc2b085334cfa146
Marshel2727 <marshelandhinosgorung.@gmail.com>
2026-04-09 00:56:00 +0800

    Menyelesaikan fitur CRUD untuk skill dan message

--- back_end/app/routes/skill.routes.py
+++ back_end/app/routes/skill.routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint,request
 from app.service.skill_service import get_all_skill,get_skill_by_id,create_skill,update_skill,delete_skill
+from app.utils.response import error_response, success_response
+from app.utils.icon_upload import save_image
+from flask_jwt_extended import jwt_required,get_jwt
 
 
 skill_bp = Blueprint('skill_bp', __name__, url_prefix='/api/skills')
@@ -8,6 +11,87 @@ skill_bp = Blueprint('skill_bp', __name__, url_prefix='/api/skills')
 def fetch_all_skills():
     skills = get_all_skill()
 
-    return jsonify({
-        status
-    })
+    return success_response(data=skills)
+
+@skill_bp.route('/<int:id>', methods=['GET'])
+def fetch_project_by_id(id):
+    
+    skill = get_skill_by_id(id)
+
+    if skill:
+        return success_response(data=skill)
+    return error_response(message='Skill tidak ditemukan!', status_code=404)
+
+@skill_bp.route('/', methods=['POST'])
+@jwt_required()
+def add_skill():
+    claims = get_jwt() # Mengambil seluruh isi token, termasuk klaim tambahan
+    if claims.get('role') != 'admin':
+        return error_response(message='Akses ditolak! Hanya admin yang boleh mengakses.', status_code=403)
+    
+    name = request.form.get('name')
+    level = request.form.get('level')
+
+    if not name:
+        return error_response(message="Nama wajib diisi.")
+    
+    image_file = request.files.get('icon_url')
+    
+    if not image_file or image_file.filename == '':
+        return error_response(message='Icon wajib diupload.')
+    
+    icon_url = save_image(image_file)
+
+    if not icon_url:
+        return error_response(message='Format gambar tidak valid')
+    
+    data = {
+        'name': name,
+        'level': level,
+        'icon_url': icon_url
+    }
+
+    new_skill = create_skill(data)
+    return success_response(data=new_skill, status_code=201)
+
+@skill_bp.route('/<int:id>', methods = ['PUT'])
+@jwt_required()
+def edit_skill(id):
+    claims = get_jwt() # Mengambil seluruh isi token, termasuk klaim tambahan
+    if claims.get('role') != 'admin':
+        return error_response(message='Akses ditolak! Hanya admin yang boleh mengakses.', status_code=403)
+    
+    name = request.form.get('name')
+    level = request.form.get('level')
+
+    data = {}
+    if name:
+        data['name'] = name
+    if level:
+        data['level'] = level
+    
+    if 'image' in request.files:
+        file = request.files.get('image')
+        new_icon = save_image(file)
+        if new_icon:
+            data['icon_url'] = new_icon
+    
+    updated_skill = update_skill(id, data)
+
+    if updated_skill:
+        return success_response(data=updated_skill, status_code=200)
+    
+    return error_response(message='Skil tidak ditemukan.', status_code=404)
+
+@skill_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def remove_skill(id):
+    claims = get_jwt() # Mengambil seluruh isi token, termasuk klaim tambahan
+    if claims.get('role') != 'admin':
+        return error_response(message='Akses ditolak! Hanya admin yang boleh mengakses.', status_code=403)
+    
+    success = delete_skill(id)
+
+    if success:
+        return success_response(message='Skill berhasil dihapus.')
+    return error_response(message='Skill tidak ditemukan.')
