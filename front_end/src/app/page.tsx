1246c6a04896d4a1ec5e3a39ebfc1d2f6fd4fa55
Marshel2727 <marshelandhinosgorung.@gmail.com>
2026-06-23 00:57:31 +0800

    Move project timeline to homepage

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import useSWR from "swr";
+import { getAllProjects } from "@/services/project";
 import Navbar from "@/components/publick/layout/Navbar";
 import Hero from "@/components/publick/layout/Hero";
+import ProjectTimeline from "@/components/publick/ProjectTimeline";
 import Footer from "@/components/publick/layout/Footer";
 import PageTransition from "@/components/publick/layout/PageTransition";
 
 export default function Home() {
+  const { data: rawProjects } = useSWR("/projects/", () => getAllProjects());
+  const projects = Array.isArray(rawProjects) ? rawProjects : [];
+
   return (
-    <div className="h-screen bg-gray-900 text-white font-sans flex flex-col overflow-y-auto lg:overflow-hidden">
+    <div className="min-h-screen bg-gray-900 text-white font-sans flex flex-col">
       <Navbar />
       
-      <main className="flex-1 flex flex-col justify-center">
+      <main className="flex-1 pt-20">
         <PageTransition>
-          <Hero />
+          <section className="flex min-h-[calc(100vh-5rem)] flex-col justify-center">
+            <Hero />
+          </section>
+          <ProjectTimeline projects={projects} />
         </PageTransition>
       </main>
 
